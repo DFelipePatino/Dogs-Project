d02b8a4f808c789fb8bd24e1f734a6d670180050
DFelipePatino <dafepa10c29@gmail.com>
2024-08-02 16:12:37 -0500

    added validation landing page

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,17 +1,53 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import Button from '@mui/material/Button';
 import { Grow } from '@mui/material';
 import Box from '@mui/material/Box';
 import LinearProgress from '@mui/material/LinearProgress';
 import './LandingPage.css';
+import Swal from 'sweetalert2';
 // import { set } from "../../../../server/src/app";
 
 const LandingPage = () => {
+    
+    const navigate = useNavigate();
 
+    const [useEffectState, setUseEffectState] = useState(false);
     const [loadingShown, setLoadingShown] = useState(false);
 
-    const navigate = useNavigate();
+    const allDogs = useSelector(state => state.dogs);
+    console.log(allDogs, 'allDogs');
+    
+    
+    useEffect(() => {
+        if (useEffectState && allDogs.length > 0) {
+            navigate('/home');
+        }
+    }, [useEffectState, allDogs.length]
+    );
+
+    const handleClick = () => {
+        setUseEffectState(true);
+        Swal.fire({
+            title: 'Atención!',
+            text: 'This instance will spin down with inactivity, which can delay requests by 50 seconds or more. Thank you for your patience. - Esta instancia se detendrá por inactividad, lo que puede retrasar las solicitudes en 50 segundos o más. Gracias por su paciencia.',
+            icon: 'warning',
+            showCancelButton: false,
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'Entendido'
+        }).then(() => {
+            if (allDogs.length === 0) {
+                setLoadingShown(true);
+            } else {
+                setLoadingShown(true);
+                setTimeout(() => {
+                    navigate('/home');
+                }, 3000);
+            }
+        });
+    };
+
 
     return (
         <div className="landing-page">
@@ -23,9 +59,7 @@ const LandingPage = () => {
                     <div className="homeButton">
                         <Button onClick={() => {
                             setLoadingShown(true);
-                            setTimeout(() => {
-                                navigate('/home')
-                            }, 2000)
+                            handleClick();
                         }}>Home</Button>
                     </div>
                 </>
